6778161105244b7710b9beefb33c2926642498c7
NguyenHung0312 <92733650+NguyenHung0312@users.noreply.github.com>
2023-09-07 23:49:13 +0700

    menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
-import { Avatar, Space } from "antd";
+import { BookOutlined, LogoutOutlined, UserOutlined } from "@ant-design/icons";
+import { Avatar, Popover, Space } from "antd";
 import React, { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Header.scss";
 const Header = () => {
   //   const { pathname } = useLocation();
@@ -46,21 +47,54 @@ const Header = () => {
       </div>
     );
   });
+
+  const content = (
+    // <Menu
+    //   defaultSelectedKeys={["1"]}
+    //   defaultOpenKeys={["sub1"]}
+    //   mode="inline"
+    //   items={items}
+    // />
+    <Space direction="vertical" className="options">
+      <Space className="options__item">
+        <BookOutlined className="options__icon" />
+        <span className="options__label">Word List</span>
+      </Space>
+      <Space className="options__item ">
+        <UserOutlined className="options__icon" />
+        <span className="options__label">Profile</span>
+      </Space>
+      <hr
+        style={{
+          padding: "0px",
+          margin: "0px",
+          color: "#ccc",
+          backgroundColor: "#ccc",
+        }}
+      />
+      <Space className="options__item sign_out">
+        <LogoutOutlined className="options__icon" />
+        <span className="options__label">Sign Out</span>
+      </Space>
+    </Space>
+  );
   return (
     <Space className={`menu ${isScroll ? "menu__scrolled" : ""}`}>
       <Space className="nav">{navItems}</Space>
-      <Space className="login">
-        Nguyen Hung
-        <Avatar
-          size="large"
-          style={{
-            backgroundColor: "#fde3cf",
-            color: "#f56a00",
-          }}
-        >
-          H
-        </Avatar>
-      </Space>
+      <Popover placement="bottom" trigger="click" content={content}>
+        <Space className="login">
+          Nguyen Hung
+          <Avatar
+            size="large"
+            style={{
+              backgroundColor: "#fde3cf",
+              color: "#f56a00",
+            }}
+          >
+            H
+          </Avatar>
+        </Space>
+      </Popover>
     </Space>
   );
 };
