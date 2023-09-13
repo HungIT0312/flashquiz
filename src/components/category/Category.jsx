d2578af38361a0f8acce28ce7fd1bde45e5ca894
HungIT0312 <nguyenhungit0312@gmail.com>
2023-09-13 22:42:31 +0700

    done basic UI about Vocabulary

--- src/components/category/Category.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Category.scss";
-import { Space } from "antd";
-const Category = () => {
-  return <></>;
-};
-
-export default Category;
--- src/pages/vocabulary/Vocabulary.jsx
+++ src/pages/vocabulary/Vocabulary.jsx
@@ -1,15 +1,19 @@
 import { Space } from "antd";
 import React from "react";
+import WordLists from "../../components/word-lists/WordLists";
 import "./Vocabulary.scss";
 const Vocabulary = () => {
   return (
-    <Space className="wrap">
+    <Space className="wrap mainwrap" direction="vertical" align="center">
       <Space className="vocab" wrap direction="vertical">
         <div className="vocab__title">English Vocabulary</div>
         <div className="vocab__intro">Categorized word lists</div>
       </Space>
-      <hr />
-      <Space></Space>
+      <Space className="word-wrap" direction="vertical">
+        <WordLists />
+        <WordLists />
+        <WordLists />
+      </Space>
     </Space>
   );
 };
